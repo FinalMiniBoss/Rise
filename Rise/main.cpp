10a49e01ea2cf07bfa448ed8f803cf2c70fabf48
Jake Watkins <jkwtkns@live.com>
2021-08-29 14:47:08 -0500

    about to copy everything from GEngine2D

--- Rise/main.cpp
+++ Rise/main.cpp
@@ -24,8 +24,8 @@ private:
 	inline static SDL_Window* _window = NULL;
 	inline static SDL_Renderer* _renderer = NULL;
 
-	inline static long long _nextFrame = 0;
-	inline static uint16_t _framerate = 60;
+	inline static std::chrono::steady_clock::time_point _nextFrame = std::chrono::steady_clock::now();
+	inline static int _framerate = 4;
 
 	inline static int _screenWidth = 640;
 	inline static int _screenHeight = 480;
@@ -65,29 +65,91 @@ public:
 
 		SDL_CaptureMouse(SDL_bool(true));
 
+		_nextFrame = std::chrono::steady_clock::now();
+
 		return;
 	}
 
 	inline static void Close()
-	{}
+	{
+		SDL_DestroyRenderer(_renderer);
+		SDL_DestroyWindow(_window);
+		SDL_Quit();
+		return;
+	}
 
 	inline static bool Update()
-	{}
+	{
+		_nextFrame += std::chrono::microseconds(1000000 / _framerate);
+
+		//stuff
+		SDL_Event e;
+		while (SDL_PollEvent(&e))
+		{
+
+			switch (e.type)
+			{
+			case SDL_KEYUP:
+				if (e.key.keysym.sym == SDLK_ESCAPE) return false;
+				break;
+
+				//
+				//Mouse Stuff
+				//
+
+			case SDL_MOUSEBUTTONDOWN:
+				//Mouse::_pressed[e.button.button - 1] = true;
+				break;
+
+			case SDL_MOUSEBUTTONUP:
+				//Mouse::_released[e.button.button - 1] = true;
+				break;
+
+			case SDL_MOUSEWHEEL:
+				//Mouse::_scroll = e.wheel.y;
+
+			default:
+				break;
+			}
+
+			if (e.type == SDL_KEYUP)
+			{
+				if (e.key.keysym.sym == SDLK_ESCAPE)
+				{
+					return false;
+				}
+			}
+			else if (e.type == SDL_QUIT) {
+				return false;
+			}
+		}
+
+
+		Draw();
+		SDL_Delay(static_cast<Uint32>((_nextFrame - std::chrono::steady_clock::now()).count() / 1000000));
+		return true;
+	}
 
 	inline static void Draw()
-	{}
+	{
+		SDL_RenderClear(_renderer);
+
+		// stuff
+
+		SDL_RenderPresent(_renderer);
+	}
 
 
 	inline static SDL_Window* Window() { return Engine::_window; }
 	inline static SDL_Renderer* Renderer() { return Engine::_renderer; }
 	inline static int Width() { return Engine::_screenWidth; }
 	inline static int Height() { return Engine::_screenHeight; }
-	inline static long long NextFrame() { return Engine::_nextFrame; }
 };
 
 int main(int argc, char* argv[]) {
 
 	Engine::Init("Boop");
+	while (Engine::Update());
 
 	std::cout << ";jklhafsd" << std::endl;
 	return 0;
